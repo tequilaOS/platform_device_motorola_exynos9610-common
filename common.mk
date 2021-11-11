e863e91f846a3bddcacb563b55f8fe0c147b7bae
Jeferson <jroliveira.oliveira301@gmail.com>
2021-11-11 15:16:35 +0100

    exynos9610: Dont dexpreopt prebuilts
    Change-Id: I443f227bfffc7edec5eb1b06bf7ccdb41da7577c
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>
    Signed-off-by: rk134 <rahul-k@bigdi.cc>

--- common.mk
+++ common.mk
@@ -191,6 +191,7 @@ PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
 # Always preopt extracted APKs to prevent extracting out of the APK
 # for gms modules.
+DONT_DEXPREOPT_PREBUILTS := true
 PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
